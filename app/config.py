16f690c4ec75b7433d4b86725f075085796be925
Michael-Musili <michaelmuttuku@gmail.com>
2022-03-12 16:15:05 +0300

    failed to debug

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = '67dc88e287b53f38'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaaccess:mapesa@localhost/mapesa'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaaccess:musili@localhost/musili'
     
 
 class DevConfig(Config):
