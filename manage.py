1244b3d6cd8593ffd9de43a37446fb56568c3ba8
Michael-Musili <michaelmuttuku@gmail.com>
2022-03-16 10:45:29 +0300

    added a new database

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app import create_app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
 from app.models import User, Pitch, Comment, Upvote, Downvote  
-app = create_app('production')
+app = create_app('development')
 manager=Manager(app)
 migrate = Migrate(app, db)
 
